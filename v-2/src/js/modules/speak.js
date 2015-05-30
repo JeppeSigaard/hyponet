8ab96036bdc0eeac8b5fcc2da18653f8c6671550
JeppeSigaard <jeppe@smatmonkey.dk>
2015-05-30 21:29:11 +0200

    nu siger den noget, da...

--- v-2/src/js/modules/speak.js
+++ v-2/src/js/modules/speak.js
@@ -2,14 +2,65 @@ var doSpeak = function(event){
     
     var target = $(event.target),
         input = target.val()
-        speak = $('.hyponet-speak');
+        speak = $('.hyponet-speak'),
+        brainLength = Object.keys(brainData).length,
+        diff = {},
+        output = '';
     
-    var brainLength = Object.keys(brainData).length;
+    // Ryd speak
+    speak.html('');
+    
+    // returner hvis input er tomt
+    if(input === ''){speak.html('');return;}
     
     
-    for (var l=0, i=brainLength; i>l; i--){
+    
+    for (var i=0, l=brainLength; i<l; i++){
         
+        // Unbreak proto
+        if (!brainData.hasOwnProperty(i)) {continue;}
         
-        speak.html(brainData[i]);
-    } 
+        if($.type(brainData[i]) !== 'string'){
+            
+            diff = JsDiff.diffWords(brainData[i].one, brainData[i].two);
+            
+            if(brainData[i].one.toLowerCase() === input.toLowerCase()){
+                
+                
+                for (var key in diff) {
+                    if (diff.hasOwnProperty(key)) {
+
+                        if(diff[key].added === true){
+
+                            output += diff[key].value + ' ';
+                            console.log(diff);
+                            
+                        }
+                    }
+                }
+                
+                
+            }
+            
+            if (brainData[i].two.toLowerCase() === input.toLowerCase()){
+                
+                for (var key in diff) {
+                    if (diff.hasOwnProperty(key)) {
+
+                        if(diff[key].removed === true){
+
+                            output += diff[key].value + ' ';
+                            console.log(diff);
+                            
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    // Hvis intet resultat, returner et spørgsmålstegn
+    if(output === ''){output = '?';}
+    speak.html(output);
 }
